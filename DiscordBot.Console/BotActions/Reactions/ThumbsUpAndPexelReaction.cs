52465b34befa3f592984886f319a29b8a6072c41
Onik Noor <O_Noor@live.co.uk>
2022-06-14 12:06:44 +0100

    Moved BotActions on its own folder

---
+++ DiscordBot.Console/BotActions/Reactions/ThumbsUpAndPexelReaction.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DiscordBot.Console.Interfaces;
+using DiscordBot.Console.Utils;
+
+namespace DiscordBot.Console.Reactions
+{
+    public class ThumbsUpAndPexelReaction : IDiscordReaction
+    {
+        public List<Emoji> Emojis()
+        {
+            return new List<Emoji>
+            {
+                Emoji.Parse("👍")
+            };
+        }
+
+        public List<Emote> Emotes()
+        {
+            return new List<Emote>
+            {
+                Emote.Parse("<:pexel:986214270297591859>")
+            };
+        }
+
+        public async Task ExecuteReactionAdded(DiscordSocketClient client, IMessage msg, IChannel channel, SocketReaction reaction)
+        {
+            if (reaction.Emote.Name == "pexel")
+            {
+                await MessageUtils.Reply(msg, "You have added a custom emote!");
+            }
+            else
+            {
+                await MessageUtils.Reply(msg, "You have added a thumbs up emote!");
+            }  
+        }
+
+        public async Task ExecuteReactionRemoved(DiscordSocketClient client, IMessage msg, IChannel channel, SocketReaction reaction)
+        {
+            if (reaction.Emote.Name == "pexel")
+            {
+                await MessageUtils.Reply(msg, "You have removed a pexel emote!");
+            }
+            else
+            {
+                await MessageUtils.Reply(msg, "You have removed a thumbs up!");
+            }
+        }
+    }
+}
