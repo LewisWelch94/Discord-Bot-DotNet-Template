d42dd56778d8497351dcd5e7d55482af4a088580
Onik Noor <O_Noor@live.co.uk>
2022-07-07 01:07:11 +0100

    Added SlashCommand Option Extension Method

---
+++ DiscordBot.Console/Utils/SlashCommandUtils.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace DiscordBot.Console.Utils
+{
+    public static class SlashCommandUtils
+    {
+        public static T? GetSlashCommandDataOption<T>(this SocketSlashCommand command, string key)
+        {
+            if (string.IsNullOrEmpty(key)) throw new ArgumentException($"'{nameof(key)}' cannot be null or empty.", nameof(key));
+            var value = command.Data.Options.Where(x => x.Name == key).FirstOrDefault();
+            return value == null ? default : (T)value.Value;
+        }
+    }
+}
+
